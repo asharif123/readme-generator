9e62ac72563334db7667bd900b8121da9808b269
Awad Sharif <awadsharif9@gmail.com>
2021-11-15 19:32:50 -0800

    added section for installation instructions

--- index.js
+++ index.js
@@ -6,23 +6,19 @@
 //2 - WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 
-//3 - WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-
-//4 - WHEN I choose a license for my application from a list of options
+//3 - WHEN I choose a license for my application from a list of options
 // THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license 
 // the application is covered under
 
-//5 - WHEN I enter my GitHub username
+//4 - WHEN I enter my GitHub username
 // THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
 
-//6 - WHEN I enter my email address
+//5 - WHEN I enter my email address
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 
-//7 - WHEN I click on the links in the Table of Contents
+//6 - WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
 
-// Use DOM elements to store each input into readme file
 
 const inquirer = require("inquirer");
 const fs = require("fs");
@@ -30,24 +26,37 @@ const fs = require("fs");
 inquirer.prompt([
     {
         type: 'input',
-        message: 'Enter the title of the readme file',
+        message: 'Enter the title of your readme file',
         name: 'title'
     },
     {
         type: 'input',
-        message: 'Enter the description title',
+        message: 'Enter the title for your Description section',
         name: 'description'
     },
     {
         type: 'input',
-        message: 'Enter information about your readme file',
+        message: 'Enter information about your readme file under Description heading',
         name: 'descriptionInformation'
+    },
+    {
+        type: 'input',
+        message: 'Enter the title for your Installation section',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions for your readme file',
+        name: 'installationInstructions'
     }
+
 
 
 ]).then((response) => { 
     fs.writeFileSync("README.md", JSON.stringify("**" + response.title + "**") + "\n\n\n");
     fs.appendFileSync("README.md", JSON.stringify('**' + response.description + "**") + "\n\n");
-    fs.appendFileSync("README.md", JSON.stringify(response.descriptionInformation));
+    fs.appendFileSync("README.md", JSON.stringify(response.descriptionInformation) + "\n\n");
+    fs.appendFileSync("README.md", JSON.stringify("**" + response.installation + "**") + "\n\n");
+    fs.appendFileSync("README.md", JSON.stringify(response.installationInstructions) + "\n\n");
 })
 .catch((error) => (console.log(error)))
